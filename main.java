83fc3545c867ea47b5eb1bb5e0ba4ff886a87166
alex <alexlazartex21@gmail.com>
2019-10-22 08:42:36 -0400

    hola mundo 2

--- main.java
+++ main.java
@@ -1,5 +1,8 @@
 public class hola{
 	public static void main(String[] args) {
-		System.out.printLn("hola mundo"):	
+		System.out.printLn("hola mundo");
+		public static void(){
+			System.out.printLn("sad");
+		}	
 	}
 }	
